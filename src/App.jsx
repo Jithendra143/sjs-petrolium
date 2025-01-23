2b0df5af8ce24b32c6ca8da91ec8909a7cc193f7
jithendra143 <jithendra43@gmail.com>
2025-01-23 18:18:22 +0530

    routs check

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import UserSettings from "./pages/UserSettings";
+
+const router = createBrowserRouter([
+  {path: '/', element: <Layout />, children: [
+    {index: true, element: <Dashboard />},
+    {path: 'user-settings', element: <UserSettings />}
+  ]}
+])
+
 function App() {
-  return (
-    <>
-      <Layout />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;

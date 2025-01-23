a584fa7bfdd45806cd14d71e958367d41c571b19
jithendra143 <jithendra43@gmail.com>
2025-01-23 18:22:46 +0530

    Added Error page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import UserSettings from "./pages/UserSettings";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
-  {path: '/', element: <Layout />, children: [
-    {index: true, element: <Dashboard />},
-    {path: 'user-settings', element: <UserSettings />}
-  ]}
-])
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "user-settings", element: <UserSettings /> },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
